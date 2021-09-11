8058d421868d7769fed85a04c518715a2f8b10c8
Vu <vuphan314@users.noreply.github.com>
2021-09-11 14:37:42 -0500

    Added option --depth to specify max depth of decision tree.

--- benchmarking/script.py
+++ benchmarking/script.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3.7
 
 import sys
-sys.path.append("../")
-
-
-from justicia import decision_tree_wrap 
-from justicia import linear_classifier_wrap 
-# from justicia import mlic_wrap
-from justicia.metrics import Metric
 
 import os
+from os.path import dirname, realpath
+
+sys.path.append(dirname(dirname(realpath(__file__))))
+
+
+from justicia import decision_tree_wrap
+from justicia import linear_classifier_wrap
+# from justicia import mlic_wrap
+from justicia.metrics import Metric
 
 
 import argparse
@@ -21,12 +24,13 @@ from data.objects.compas import Compas
 from data.objects.communities import Communities_and_Crimes
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-v", "--verbose", action='store_true')
 parser.add_argument("--dataset", type=str, default="titanic", choices=['titanic', 'adult', 'ricci', 'german','bank', 'compas', 'communities'])
 parser.add_argument("--config", type=int, default=0)
 parser.add_argument("--model", default="dt", type=str, choices=['dt'])
 parser.add_argument("--encoding", nargs='+', default=['Learn'])
+parser.add_argument("--depth", type=int, default=4, help='max depth for decision-tree classifier')
 args = parser.parse_args()
 
 
@@ -47,7 +51,7 @@ elif(args.dataset == "bank"):
     datasetObj = Bank(verbose=verbose,config=args.config)
 elif(args.dataset == "communities"):
     datasetObj = Communities_and_Crimes(verbose=verbose,config=args.config)
-    
+
 else:
     raise ValueError(args.dataset + " is not a defined dataset")
 
@@ -87,17 +91,17 @@ if(model_name == 'svm-linear'):
     model, data_train, data_test,sensitive_attributes, y_train, y_test = linear_classifier_wrap.init(datasetObj, classifier=model_name, repaired=False, verbose=False, compute_equalized_odds=True)
 
 if(model_name == 'dt'):
-    model, data_train, data_test,sensitive_attributes, y_train, y_test = decision_tree_wrap.init(datasetObj, repaired=False, verbose=False, compute_equalized_odds=True, depth=4)
+    model, data_train, data_test,sensitive_attributes, y_train, y_test = decision_tree_wrap.init(datasetObj, repaired=False, verbose=False, compute_equalized_odds=True, depth=args.depth)
 
-# if(model_name == "CNF"):    
+# if(model_name == "CNF"):
 #     model, data_train, data_test,sensitive_attributes, y_train, y_test = mlic_wrap.init(datasetObj, repaired=False, verbose=False, compute_equalized_odds=True)
 
 
-print("Sensitive attributes:", sensitive_attributes)  
-for i in range(1):   
-    
+print("Sensitive attributes:", sensitive_attributes)
+for i in range(1):
 
-    
+
+
     for encoding in args.encoding:
         metric = Metric(model[i], data_test[i], sensitive_attributes, mediator_attributes=[], major_group={}, verbose=args.verbose, encoding=encoding, filename=filename).compute()
         print(metric)
@@ -105,5 +109,3 @@ for i in range(1):
 
 
 os.system("rm " + filename + "*")
-
-        
